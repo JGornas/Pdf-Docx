41b620df77246b99ba86836f025df3d1389b8d79
Jake <cavalorn12@gmail.com>
2019-04-12 12:02:34 +0200

    V 1.21  - Added wspólnicy&zarząd  - Started rewriting ifs to functions

--- new_parser.py
+++ new_parser.py
@@ -1,20 +1,32 @@
 from subprocess import call
 import os
-
+import re
 
 class PdfParser:
     def __init__(self, pdf_filename="odpis_aktualny_1.pdf"):
         self.pdf_filename = pdf_filename
         self.txt_filename = f"{pdf_filename.strip('.pdf')}.txt"
 
-        self.variables = {}
+        self.datafields = {}
         self.parsed_state = {
-                            "Firma, pod którą spółka działa": False, "Oznaczenie sądu": False, "Siedziba": False,
+                            "Firma, pod którą spółka działa": False,  "Oznaczenie sądu": False, "Siedziba": False,
                             "KRS": False, "REGON/NIP": False, "Adres": False, "Forma Prawna": False,
-                            "Kapitał Zakładowy": False, "Wspólnicy": 1, "Członków Zarządu": 0,
+                            "Kapitał Zakładowy": False, "Wspólnicy": False, "Zarząd": False,
                              }
+        self.active = 0
+        self.unicode = "\u00D3\u0104\u0106\u0141\u0143\u015A\u0179\u017B"
 
-    def load_pdf(self, remove_txt=True, debug=False):
+    def search_byindex(self, index, paragraph, paragraphs, cell_name):
+        index = paragraphs.index(paragraph) + index
+        if paragraphs[index + 1] is not "":  # for multiline text cells
+            paragraphs[index] += f" {paragraphs[index + 1]}"
+        self.datafields[cell_name] = paragraphs[index]
+        self.parsed_state[cell_name] = True
+
+    def search_byloop(self):
+        pass
+
+    def load_pdf(self, remove_txt=True, debug=False, debug_range=(0, 500)):
         call([os.path.join("venv", "Scripts", "python.exe"),
               os.path.join("venv", "Scripts", "pdf2txt.py"),
               os.path.join("pdf", f"{self.pdf_filename}"),
@@ -25,35 +37,31 @@ class PdfParser:
             os.remove(os.path.join("txt", f"{self.txt_filename}"))
         if debug:
             for counter, paragraph in enumerate(paragraphs):
-                if counter < 400:
+                if debug_range[0] < counter < debug_range[1]:
                     print(counter, paragraph)
-        for paragraph in paragraphs:
 
+        for paragraph in paragraphs:
             if paragraph == "Oznaczenie sądu" and self.parsed_state["Oznaczenie sądu"] is False:
-                index = paragraphs.index(paragraph) + 4
-                oznaczenie = paragraphs[index]
-                check = paragraphs[index + 1]
-                if check is not "":  # checks for a newline (bugfix)
-                    oznaczenie += f" {check}"
-                self.variables["Oznaczenie sądu"] = oznaczenie
+                #index = paragraphs.index(paragraph) + 4
+                #if paragraphs[index + 1] is not "":  # for multiline text cells
+                #    paragraphs[index] += f" {paragraphs[index + 1]}"
+                #self.datafields["Oznaczenie sądu"] = paragraphs[index]
+                self.search_byindex(4, paragraph, paragraphs, "Oznaczenie sądu")
 
             if paragraph.startswith("3.Firma,") and self.parsed_state["Firma, pod którą spółka działa"] is False:
                 index = paragraphs.index(paragraph) + 2
-                self.variables["Firma, pod którą spółka działa"] = paragraphs[index]
-                self.parsed_state["3.Firma"] = True
+                self.datafields["Firma, pod którą spółka działa"] = paragraphs[index]
+                self.parsed_state["Firma, pod którą spółka działa"] = True
 
             if paragraph.startswith("1.Siedziba") and self.parsed_state["Siedziba"] is False:
                 index = paragraphs.index(paragraph) + 4
                 kraj, wojewodztwo, powiat, gmina, miejscowosc = paragraphs[index].split(",")
                 miejscowosc = miejscowosc.strip("  miejsc.")
-                if miejscowosc == "":  # checks for a newline
-                    index = index + 1
-                    miejscowosc = paragraphs[index]
-                self.variables["Siedziba_Kraj"] = kraj.strip("kraj ")
-                self.variables["Siedziba_Wojewodztwo"] = wojewodztwo.strip("woj. ")
-                self.variables["Siedziba_Powiat"] = powiat.strip("  powiat ")
-                self.variables["Siedziba_Gmina"] = gmina.strip("  gmina ")
-                self.variables["Siedziba_Miejscowosc"] = miejscowosc
+                self.datafields["Siedziba_Kraj"] = kraj.strip("kraj ")
+                self.datafields["Siedziba_Wojewodztwo"] = wojewodztwo.strip("woj. ")
+                self.datafields["Siedziba_Powiat"] = powiat.strip("  powiat ")
+                self.datafields["Siedziba_Gmina"] = gmina.strip("  gmina ")
+                self.datafields["Siedziba_Miejscowosc"] = miejscowosc
                 self.parsed_state["Siedziba"] = True
 
             if paragraph.startswith("2.Adres") and self.parsed_state["Adres"] is False:
@@ -63,80 +71,134 @@ class PdfParser:
                 if miejscowosc == "":  # checks for a newline
                     index = index + 1
                     miejscowosc = paragraphs[index]
-                self.variables["Adres_Ulica"] = ulica.strip("ul.  ")
-                self.variables["Adres_Numer"] = numer.strip(" nr ")
-                self.variables["Adres_Lokal"] = lokal.strip("lok. ")
-                self.variables["Adres_Miejscowosc"] = miejscowosc
-                self.variables["Adres_Kod"] = kod.strip(" kod ")
-                self.variables["Adres_Poczta"] = poczta.strip(" poczta ")
-                self.variables["Adres_Kraj"] = kraj.strip("kraj ")
+                self.datafields["Adres_Ulica"] = ulica.strip("ul.  ")
+                self.datafields["Adres_Numer"] = numer.strip(" nr ")
+                self.datafields["Adres_Lokal"] = lokal.strip("lok. ")
+                self.datafields["Adres_Miejscowosc"] = miejscowosc
+                self.datafields["Adres_Kod"] = kod.strip(" kod ")
+                self.datafields["Adres_Poczta"] = poczta.strip(" poczta ")
+                self.datafields["Adres_Kraj"] = kraj.strip("kraj ")
                 self.parsed_state["Adres"] = True
 
             if paragraph.startswith("Numer KRS") and self.parsed_state["KRS"] is False:
-                self.variables["KRS"] = paragraph.split()[-1]
+                self.datafields["KRS"] = paragraph.split()[-1]
                 self.parsed_state["KRS"] = True
 
             if paragraph.startswith("2.Numer REGON/NIP") and self.parsed_state["REGON/NIP"] is False:
                 index = paragraphs.index(paragraph) + 2
                 regon, nip = paragraphs[index].split(",")
-                self.variables["NIP"] = nip.strip("  NIP: ").split()[0].strip("---")
-                self.variables["REGON"] = regon.strip("REGON: ").split()[0]
+                self.datafields["NIP"] = nip.strip("  NIP: ").split()[0].strip("---")
+                self.datafields["REGON"] = regon.strip("REGON: ").split()[0]
                 self.parsed_state["KRS"] = True
 
             if paragraph.startswith("1.Oznaczenie formy prawnej") and self.parsed_state["Forma Prawna"] is False:
                 index = paragraphs.index(paragraph) + 2
-                self.variables["Forma Prawna"] = paragraphs[index]
+                self.datafields["Forma Prawna"] = paragraphs[index]
                 self.parsed_state["Forma Prawna"] = True
 
             if paragraph.startswith("1.Wysokość kapitału zakładowego"):
                 index = paragraphs.index(paragraph) + 2
-                self.variables["Kapitał Zakładowy"] = paragraphs[index]
+                self.datafields["Kapitał Zakładowy"] = paragraphs[index]
                 self.parsed_state["Kapitał Zakładowy"] = True
 
-            if paragraph.startswith("Rubryka 7 - Dane wspólników"):
-                index = paragraphs.index(paragraph) + 2
-                count = 0
+            if paragraph.startswith("1.Nazwisko / Nazwa lub firma"):
+                self.parsed_state["Wspólnicy"] = True
+                self.active += 1
+                self.datafields[f"Wspólnik {self.active}"] = {}
+                pattern = r"^[A-Z\u00D3\u0104\u0106\u0141\u0143\u015A\u0179\u017B]+"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
                 while True:
-                    if paragraphs[index] == str(int(count) + 1):
-                        count += 1
-                        index += 2
-                    else:
+                    index += 1
+                    if re.match(pattern, paragraphs[index]):
+                        self.datafields[f"Wspólnik {self.active}"]["Nazwisko/Nazwa"] = paragraphs[index].rstrip()
                         break
-                self.variables["Wspólników"] = count
-                for i in range(1, count+1):
-                    self.variables[f"Wspólnik: {i}"] = {"Nazwisko / Nazwa lub firma": "", "Imiona": "",
-                                                        "Numer PESEL/REGON": "", "Numer KRS": "",
-                                                        "Posiadane Udziały": "", "Complete": False}
 
-            if paragraph.startswith("1.Nazwisko / Nazwa lub firma") and self.parsed_state["Wspólnicy"] < self.variables["Wspólników"]+1:
-                print(self.parsed_state["Wspólnicy"], self.variables["Wspólników"])
-                index = paragraphs.index(paragraph) + 2
-                paragraph = paragraphs[index]
-                self.variables[f"Wspólnik: {self.parsed_state['Wspólnicy']}"]["Nazwisko / Nazwa lub firma"] = paragraph
-                self.parsed_state["Wspólnicy"] += 1
-
-
-            if paragraph.startswith("Dane osób wchodzących w skład organu"):
-                index = paragraphs.index(paragraph) + 2
-                count = 0
+            if paragraph.startswith("2.Imiona") and self.parsed_state["Wspólnicy"]:
+                pattern = rf"[A-Z{self.unicode}]+\s[A-Z{self.unicode}]+$|^[A-Z{self.unicode}]+$|^[*]+$"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
                 while True:
-                    if paragraphs[index] == str(int(count) + 1):
-                        count += 1
-                        index += 2
-                    else:
+                    index += 1
+                    if re.match(pattern, paragraphs[index]):
+                        self.datafields[f"Wspólnik {self.active}"]["Imiona"] = paragraphs[index].rstrip()
+                        paragraphs[index] = "parsed"
                         break
-                self.variables["Członków Zarządu"] = count
-                for i in range(1, count+1):
-                    self.variables[f"Członek Zarządu: {i}"] = {"Nazwisko / Nazwa lub firma": "", "Imiona": "",
-                                                               "Numer PESEL/REGON": "", "Numer KRS": "",
-                                                               "Funkcja": ""}
 
-        return self.variables
+            if paragraph.startswith("3.Numer PESEL/REGON") and self.parsed_state["Wspólnicy"]:
+                pattern = r"[-]+|[0-9]{9,11}"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
+                while True:
+                    index += 1
+                    if re.match(pattern, paragraphs[index]):
+                        self.datafields[f"Wspólnik {self.active}"]["PESEL/REGON"] = paragraphs[index].rstrip()
+                        paragraphs[index] = "parsed"
+                        break
+                    if index == len(paragraphs):
+                        break
+
+            if paragraph.startswith("4.Numer KRS") and self.parsed_state["Wspólnicy"]:
+                pattern = r"[-]+|[*]+|[0-9]{10}$"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
+                while True:
+                    index += 1
+                    if re.match(pattern, paragraphs[index]):
+                        self.datafields[f"Wspólnik {self.active}"]["KRS"] = paragraphs[index].rstrip()
+                        paragraphs[index] = "parsed"
+                        break
+                    if index == len(paragraphs):
+                        break
+
+            if paragraph.startswith("5.Posiadane przez wspólnika udziały"):
+                index = paragraphs.index(paragraph)
+                line_1 = paragraphs[index + 2].strip(" ")
+                line_2 = paragraphs[index + 3].strip(" ")
+                if line_2:
+                    self.datafields[f"Wspólnik {self.active}"]["Udziały"] = f"{line_1} {line_2}"
+                else:
+                    self.datafields[f"Wspólnik {self.active}"]["Udziały"] = f"{line_1}"
+            if paragraph == "ZARZĄD":
+                self.parsed_state["Wspólnicy"] = False
+                self.parsed_state["Zarząd"] = True
+                self.active = 0
+
+            if paragraph.startswith("1.Nazwisko / Nazwa lub Firma") and self.parsed_state["Zarząd"]:
+                self.active += 1
+                self.datafields[f"Zarząd {self.active}"] = {}
+                pattern = r"^[A-Z\u00D3\u0104\u0106\u0141\u0143\u015A\u0179\u017B]+"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
+                while True:
+                    index += 1
+                    if re.match(pattern, paragraphs[index].rstrip()):
+                        self.datafields[f"Zarząd {self.active}"]["Nazwisko/Nazwa"] = paragraphs[index].rstrip()
+                        break
+
+            if paragraph.startswith("2.Imiona") and self.parsed_state["Zarząd"]:
+                pattern = rf"^[A-Z{self.unicode}]+\s[A-Z{self.unicode}]+$|^[A-Z{self.unicode}]+$|^[*]+$"
+                index = paragraphs.index(paragraph)
+                paragraphs[index] = "parsed"
+                while True:
+                    if re.match(pattern, paragraphs[index].rstrip()):
+                        self.datafields[f"Zarząd {self.active}"]["Imiona"] = paragraphs[index].rstrip()
+                        paragraphs[index] = "parsed"
+                        break
+                    index += 1
+
+            if paragraph.startswith("5.Funkcja w organie ") and self.parsed_state["Zarząd"]:
+                paragraph = paragraph.strip("5.Funkcja w organie reprezentującym ")
+                self.datafields[f"Zarząd {self.active}"]["Funkcja"] = paragraph
+
+            if paragraph.startswith("Rubryka 2 - Organ nadzoru"):
+                self.parsed_state["Zarząd"] = False
+        return self.datafields
 
 
 if __name__ == "__main__":
-    parser = PdfParser()
-    variables = parser.load_pdf(debug=True)
-    for key in variables:
-        print(f"{key} : {variables[key]}")
+    parser = PdfParser(pdf_filename="odpis_aktualny_5.pdf")
+    datafields = parser.load_pdf(debug=True, debug_range=(200, 365))
+    for key in datafields:
+        print(f"{key} : {datafields[key]}")
 
--- tests.py
+++ tests.py
@@ -4,31 +4,154 @@ from new_parser import PdfParser
 
 class TestPdfParser(unittest.TestCase):
 
-    def test_extract_pdf(self):
+    def test_pdf_1(self):
         parser = PdfParser()
         parser.pdf_filename = "odpis_aktualny_1.pdf"
-        variables = parser.load_pdf(remove_txt=False, debug=False)
-        self.assertEqual(variables["Firma, pod którą spółka działa"],
+        datafields = parser.load_pdf(remove_txt=False, debug=False)
+        self.assertEqual(datafields["Firma, pod którą spółka działa"],
                          "MODELARNIA SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
-        self.assertEqual(variables["Oznaczenie sądu"],
+        self.assertEqual(datafields["Oznaczenie sądu"],
                          "SĄD REJONOWY POZNAŃ - NOWE MIASTO I WILDA W POZNANIU,"
                          " VIII WYDZIAŁ GOSPODARCZY KRAJOWEGO REJESTRU SĄDOWEGO")
-        self.assertEqual(variables["Siedziba_Kraj"], "POLSKA")
-        self.assertEqual(variables["Siedziba_Wojewodztwo"], "WIELKOPOLSKIE")
-        self.assertEqual(variables["Siedziba_Powiat"], "POZNAŃ")
-        self.assertEqual(variables["Siedziba_Gmina"], "POZNAŃ")
-        self.assertEqual(variables["Siedziba_Miejscowosc"], "POZNAŃ")
-        self.assertEqual(variables["KRS"], "0000551026")
-        self.assertEqual(variables["NIP"], "7831724589")
-        self.assertEqual(variables["REGON"], "361131194")
+        self.assertEqual(datafields["Forma Prawna"], "SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
+        self.assertEqual(datafields["KRS"], "0000551026")
+        self.assertEqual(datafields["NIP"], "7831724589")
+        self.assertEqual(datafields["REGON"], "361131194")
 
-        self.assertEqual(variables["Adres_Ulica"], "3 MAJA")
-        self.assertEqual(variables["Adres_Numer"], "49C")
-        self.assertEqual(variables["Adres_Lokal"], "2A")
-        self.assertEqual(variables["Adres_Miejscowosc"], "POZNAŃ")
-        self.assertEqual(variables["Adres_Kod"], "61-728")
-        self.assertEqual(variables["Adres_Poczta"], "POZNAŃ")
-        self.assertEqual(variables["Adres_Kraj"], "POLSKA")
+        self.assertEqual(datafields["Siedziba_Kraj"], "POLSKA")
+        self.assertEqual(datafields["Siedziba_Wojewodztwo"], "WIELKOPOLSKIE")
+        self.assertEqual(datafields["Siedziba_Powiat"], "POZNAŃ")
+        self.assertEqual(datafields["Siedziba_Gmina"], "POZNAŃ")
+        self.assertEqual(datafields["Siedziba_Miejscowosc"], "POZNAŃ")
 
-        self.assertEqual(variables["Forma Prawna"], "SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
-        self.assertEqual(variables["Kapitał Zakładowy"], "6 000,00 ZŁ")
+        self.assertEqual(datafields["Adres_Ulica"], "3 MAJA")
+        self.assertEqual(datafields["Adres_Numer"], "49C")
+        self.assertEqual(datafields["Adres_Lokal"], "2A")
+        self.assertEqual(datafields["Adres_Miejscowosc"], "POZNAŃ")
+        self.assertEqual(datafields["Adres_Kod"], "61-728")
+        self.assertEqual(datafields["Adres_Poczta"], "POZNAŃ")
+        self.assertEqual(datafields["Adres_Kraj"], "POLSKA")
+
+        self.assertEqual(datafields["Kapitał Zakładowy"], "6 000,00 ZŁ")
+        self.assertEqual(datafields["Wspólnik 1"]["Nazwisko/Nazwa"], "STĘPIEŃ")
+        self.assertEqual(datafields["Wspólnik 1"]["Imiona"], "MIKOŁAJ DANIEL")
+        self.assertEqual(datafields["Wspólnik 1"]["PESEL/REGON"], "81121003839")
+        self.assertEqual(datafields["Wspólnik 1"]["KRS"], "*****")
+        self.assertEqual(datafields["Wspólnik 1"]["Udziały"], "40 UDZIAŁÓW O ŁĄCZNEJ WARTOŚCI 2.000,00 ZŁ")
+
+        self.assertEqual(datafields["Wspólnik 2"]["Nazwisko/Nazwa"], "GOLEC")
+        self.assertEqual(datafields["Wspólnik 2"]["Imiona"], "NORBERT TOMASZ")
+        self.assertEqual(datafields["Wspólnik 2"]["PESEL/REGON"], "81082106275")
+        self.assertEqual(datafields["Wspólnik 2"]["KRS"], "*****")
+        self.assertEqual(datafields["Wspólnik 2"]["Udziały"], "40 UDZIAŁÓW O ŁĄCZNEJ WARTOŚCI 2.000,00 ZŁ")
+
+        self.assertEqual(datafields["Wspólnik 3"]["Nazwisko/Nazwa"], "ANDRYS")
+        self.assertEqual(datafields["Wspólnik 3"]["Imiona"], "KORNEL FILIP")
+        self.assertEqual(datafields["Wspólnik 3"]["PESEL/REGON"], "81103103078")
+        self.assertEqual(datafields["Wspólnik 3"]["KRS"], "*****")
+        self.assertEqual(datafields["Wspólnik 3"]["Udziały"], "40 UDZIAŁÓW O ŁĄCZNEJ WARTOŚCI 2.000,00 ZŁ")
+
+        self.assertEqual(datafields["Zarząd 1"]["Nazwisko/Nazwa"], "STĘPIEŃ")
+        self.assertEqual(datafields["Zarząd 1"]["Imiona"], "MIKOŁAJ DANIEL")
+        self.assertEqual(datafields["Zarząd 1"]["Funkcja"], "CZŁONEK ZARZĄDU")
+
+        self.assertEqual(datafields["Zarząd 2"]["Nazwisko/Nazwa"], "GOLEC")
+        self.assertEqual(datafields["Zarząd 2"]["Imiona"], "NORBERT TOMASZ")
+        self.assertEqual(datafields["Zarząd 2"]["Funkcja"], "CZŁONEK ZARZĄDU")
+
+        self.assertEqual(datafields["Zarząd 3"]["Nazwisko/Nazwa"], "ANDRYS")
+        self.assertEqual(datafields["Zarząd 3"]["Imiona"], "KORNEL FILIP")
+        self.assertEqual(datafields["Zarząd 3"]["Funkcja"], "CZŁONEK ZARZĄDU")
+
+
+    def test_pdf_3(self):
+        parser = PdfParser()
+        parser.pdf_filename = "odpis_aktualny_3.pdf"
+        datafields = parser.load_pdf(remove_txt=False, debug=False)
+        self.assertEqual(datafields["Firma, pod którą spółka działa"],
+                         "SECO/WARWICK SERVICES SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
+        self.assertEqual(datafields["Oznaczenie sądu"],
+                         "SĄD REJONOWY  W ZIELONEJ GÓRZE,"
+                         " VIII WYDZIAŁ GOSPODARCZY KRAJOWEGO REJESTRU SĄDOWEGO")
+        self.assertEqual(datafields["Siedziba_Kraj"], "POLSKA")
+        self.assertEqual(datafields["Siedziba_Wojewodztwo"], "LUBUSKIE")
+        self.assertEqual(datafields["Siedziba_Powiat"], "ŚWIEBODZIŃSKI")
+        self.assertEqual(datafields["Siedziba_Gmina"], "ŚWIEBODZIN")
+        self.assertEqual(datafields["Siedziba_Miejscowosc"], "ŚWIEBODZIN")
+        self.assertEqual(datafields["KRS"], "0000590289")
+        self.assertEqual(datafields["NIP"], "9271938125")
+        self.assertEqual(datafields["REGON"], "363190781")
+
+        self.assertEqual(datafields["Adres_Ulica"], "ZACHODNIA")
+        self.assertEqual(datafields["Adres_Numer"], "76")
+        self.assertEqual(datafields["Adres_Lokal"], "---")
+        self.assertEqual(datafields["Adres_Miejscowosc"], "ŚWIEBODZIN")
+        self.assertEqual(datafields["Adres_Kod"], "66-200")
+        self.assertEqual(datafields["Adres_Poczta"], "ŚWIEBODZIN")
+        self.assertEqual(datafields["Adres_Kraj"], "POLSKA")
+
+        self.assertEqual(datafields["Forma Prawna"], "SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
+        self.assertEqual(datafields["Kapitał Zakładowy"], "2 410 000,00 ZŁ")
+        self.assertEqual(datafields["Wspólnik 1"]["Nazwisko/Nazwa"], "SECO/WARWICK SPÓŁKA AKCYJNA")
+        self.assertEqual(datafields["Wspólnik 1"]["Imiona"], "*****")
+        self.assertEqual(datafields["Wspólnik 1"]["PESEL/REGON"], "970011679")
+        self.assertEqual(datafields["Wspólnik 1"]["KRS"], "0000271014")
+        self.assertEqual(datafields["Wspólnik 1"]["Udziały"], "24.100 UDZIAŁÓW O ŁĄCZNEJ WARTOŚCI 2.410.000,00 ZŁ")
+
+        self.assertEqual(datafields["Zarząd 1"]["Nazwisko/Nazwa"], "SZADKOWSKI")
+        self.assertEqual(datafields["Zarząd 1"]["Imiona"], "ROBERT")
+        self.assertEqual(datafields["Zarząd 1"]["Funkcja"], "CZŁONEK ZARZĄDU")
+
+        self.assertEqual(datafields["Zarząd 2"]["Nazwisko/Nazwa"], "TOMASZEWSKA KOWALSKA")
+        self.assertEqual(datafields["Zarząd 2"]["Imiona"], "KATARZYNA ROBERTA")
+        self.assertEqual(datafields["Zarząd 2"]["Funkcja"], "CZŁONEK ZARZĄDU")
+
+    def test_pdf_mouser(self):
+        parser = PdfParser()
+        parser.pdf_filename = "odpis_aktualny_Mouser.pdf"
+        datafields = parser.load_pdf(remove_txt=False, debug=False)
+        self.assertEqual(datafields["Firma, pod którą spółka działa"],
+                         "MOUSER POLAND SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
+        self.assertEqual(datafields["Oznaczenie sądu"],
+                         "------")
+        self.assertEqual(datafields["Siedziba_Kraj"], "POLSKA")
+        self.assertEqual(datafields["Siedziba_Wojewodztwo"], "DOLNOŚLĄSKIE")
+        self.assertEqual(datafields["Siedziba_Powiat"], "WROCŁAW")
+        self.assertEqual(datafields["Siedziba_Gmina"], "WROCŁAW")
+        self.assertEqual(datafields["Siedziba_Miejscowosc"], "WROCŁAW")
+        self.assertEqual(datafields["KRS"], "0000758939")
+        self.assertEqual(datafields["NIP"], "8992853938")
+        self.assertEqual(datafields["REGON"], "381866678")
+
+        self.assertEqual(datafields["Adres_Ulica"], "WYŚCIGOWA")
+        self.assertEqual(datafields["Adres_Numer"], "56E")
+        self.assertEqual(datafields["Adres_Lokal"], "---")
+        self.assertEqual(datafields["Adres_Miejscowosc"], "WROCŁAW")
+        self.assertEqual(datafields["Adres_Kod"], "53-012")
+        self.assertEqual(datafields["Adres_Poczta"], "WROCŁAW")
+        self.assertEqual(datafields["Adres_Kraj"], "POLSKA")
+
+        self.assertEqual(datafields["Forma Prawna"], "SPÓŁKA Z OGRANICZONĄ ODPOWIEDZIALNOŚCIĄ")
+        self.assertEqual(datafields["Kapitał Zakładowy"], "5 000,00 ZŁ")
+
+        self.assertEqual(datafields["Wspólnik 1"]["Nazwisko/Nazwa"], "MOUSER ELECTRONICS INC.")
+        self.assertEqual(datafields["Wspólnik 1"]["Imiona"], "*****")
+        self.assertEqual(datafields["Wspólnik 1"]["PESEL/REGON"], "---")
+        self.assertEqual(datafields["Wspólnik 1"]["KRS"], "----------")
+        self.assertEqual(datafields["Wspólnik 1"]["Udziały"],
+                         "49 (CZTERDZIEŚCI DZIEWIĘĆ) UDZIAŁÓW O ŁĄCZNEJ WYSOKOŚCI 4.950,00 (CZTERY TYSIĄCE"
+                         " DZIEWIĘĆSET PIĘĆDZIESIĄT) ZŁOTYCH")
+
+        self.assertEqual(datafields["Zarząd 1"]["Nazwisko/Nazwa"], "BROWN")
+        self.assertEqual(datafields["Zarząd 1"]["Imiona"], "SCOTT LESLIE")
+        self.assertEqual(datafields["Zarząd 1"]["Funkcja"], "PREZES ZARZĄDU")
+
+
+
+
+
+
+
+
+
+
