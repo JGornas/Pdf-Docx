2c107b4a6a65f81dc638c7f02820d37f80361efd
Jake <cavalorn12@gmail.com>
2019-04-14 20:19:11 +0200

    main.py pathing

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ VERSION = 1.0
 
 print(f">>> Now running: Pdf-Docx {VERSION}\n ")
 
-for pdf_file in os.listdir("pdf\\"):
+for pdf_file in os.listdir(os.path.join("pdf", "")):
     try:
         if pdf_file.endswith(".pdf"):
             parser = DocParser(pdf_file)  # initiate parser object with pdf file
-            if parser.docx_filename not in os.listdir("docx\\"):
-                if parser.txt_filename not in os.listdir("txt\\"):
+            if parser.docx_filename not in os.listdir(os.path.join("docx", "")):
+                if parser.txt_filename not in os.listdir(os.path.join("txt", "")):
                     parser.extract_pdf()
                     parser.parse_all()  # runs a method pack for all parsing
                 else:
